c7d6c46b65bec0e3c1b437233e405c07c5b75c76
Billel <b.sahouli@hotmail.fr>
2021-04-10 23:48:46 +0200

    affichage compte en cour de traitement & les compte validé sont marquer comme validé & affichage menu lié a ADMIN si admin conecter & reglage date naissance & correction sur la faille de double creation compte bancaire qui générais une erreur et qui rendais le site innopérant.

---
+++ src/Form/IsDeleteType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IsDeleteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('name')
+            ->add('lastname')
+            ->add('birthday')
+            ->add('adress')
+            ->add('country')
+            ->add('city')
+            ->add('image')
+            ->add('isDelete')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
